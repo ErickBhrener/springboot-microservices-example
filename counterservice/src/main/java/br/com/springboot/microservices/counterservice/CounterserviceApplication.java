78cfcc918bd6b053f1d8397e5a364c1295c5d44c
Erick Bhrener <erick.silva@rp10606.semace.local>
2018-11-09 15:43:48 -0300

    Registryservice created

--- counterservice/src/main/java/br/com/springboot/microservices/counterservice/CounterserviceApplication.java
+++ counterservice/src/main/java/br/com/springboot/microservices/counterservice/CounterserviceApplication.java
@@ -2,10 +2,12 @@ package br.com.springboot.microservices.counterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="br.com.springboot.microservices")
+@EnableDiscoveryClient
 public class CounterserviceApplication {
 
 	public static void main(String[] args) {
---
+++ gatewayservice/src/main/java/br/com/springboot/microservices/gatewayservice/GatewayserviceApplication.java
@@ -0,0 +1,12 @@
+package br.com.springboot.microservices.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserviceApplication.class, args);
+	}
+}
