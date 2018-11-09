1582094106055ad302bf680c89f5d80fcd958664
Erick Bhrener <erick.silva@rp10606.semace.local>
2018-11-09 17:03:11 -0300

    Gatewayservice, registryservice and security added

--- gatewayservice/src/main/java/br/com/springboot/microservices/gatewayservice/GatewayserviceApplication.java
+++ gatewayservice/src/main/java/br/com/springboot/microservices/gatewayservice/GatewayserviceApplication.java
@@ -2,8 +2,12 @@ package br.com.springboot.microservices.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableZuulProxy
+@ComponentScan(basePackages = "br.com.springboot.microservices")
 public class GatewayserviceApplication {
 
 	public static void main(String[] args) {
